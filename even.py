96a58de13a039ba3754e84dadf75e5f77743d272
Anusuya123 <41141241+Anusuya123@users.noreply.github.com>
2018-07-12 14:40:41 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,6 +1,9 @@
 n=int(input())
-if(n%2==0):
-	print("Number is Even")
+if(n>=1 and n<=100000)
+ if(n%2==0):
+	print("Even")
+ elif(n%2==1):
+	print("Odd")
 else:
-	print("Number is Odd")
+   print("invalid")
 	
